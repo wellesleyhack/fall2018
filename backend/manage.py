ae62000dc6eee3da30f7a7b132a5ceb593ad1183
Yada Pruksachatkun <-f>
2017-08-25 13:39:37 -0400

    added backend Flask and AWS

---
+++ backend/manage.py
@@ -0,0 +1,45 @@
+import json
+import time
+from boto.dynamodb2.table import Table
+from flask import Flask
+# dynamodb is only for smaller key-value store, whereas s3 is for
+# larger file stores.
+
+conn_dynamodb = Table('WHACK_registered_userss')
+app = Flask(__name__)
+
+@app.route('/create', methods=['POST'])
+def create():
+    data_file = request.files.get('resume')
+    data_values = request.form
+    file_name = data_file.filename
+    conn = boto.s3.connection.S3Connection(aws_access_key_id, aws_secret_access_key)
+    bucket = conn.get_bucket(settings.BUCKET_NAME)
+    k = Key(bucket)
+    k.key = file_name
+    # k.set_contents_from_file(data_file)
+    k.set_contents_from_string(data_file.read())
+    response = conn_dynamodb.put_item(
+       Item={
+            'name': data_values.name,
+            'email':data_values.email,
+            'school':data_values.school,
+            'major': data_values.major,
+            'graduation_year': data_values.email,
+            'ethnicity' : data_values.ethnicity,
+            'gender_identity': data_values.gender_identity,
+            'sexual_orientation': data_values.sexual_orientation,
+            'resume': data_values.file_name,
+            'links': data_values.links,
+            'first_hackathon': data_values.first_hackathon,
+            'special_accomodations': data_values.special_accomodations,
+            'other_notes': data_values.other_notes
+        }
+    )
+    # right now, we need to query from AWS and S3. Googel Sheets API.
+    # return jsonify(name=file_name)
+    return jsonify(name=file_name, response=response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
