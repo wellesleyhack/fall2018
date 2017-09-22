210b9c93bece70d5ffa82bffd99d1a0a39de75c8
Yada Pruksachatkun <-f>
2017-09-21 21:20:34 -0400

    made more secure changed password

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #for gmail or google apps
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_USERNAME = 'hello@wellesleyhacks.org'
-MAIL_PASSWORD = "psprxsgjzobnnwbm"
+MAIL_PASSWORD = os.environ["WHACK_FINAL_PASSWORD"]
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
 MAIL_PORT = 465
--- manage.py
+++ manage.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 app.config.update(dict({
 "MAIL_SERVER" : 'smtp.gmail.com',
 "MAIL_USERNAME" : 'hello@wellesleyhacks.org',
-"MAIL_PASSWORD" : "psprxsgjzobnnwbm",
+"MAIL_PASSWORD" : os.environ["WHACK_FINAL_PASSWORD"],
 "MAIL_USE_TLS": False,
 "MAIL_USE_SSL" : True,
 "MAIL_PORT": 465}))
