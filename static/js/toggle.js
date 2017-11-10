3313d8bc51b316f854a978cc17b6e6df795454ed
AMY JIN <zjin@wellesley.edu>
2017-11-10 13:17:46 -0500

    testing

---
+++ static/js/toggle.js
@@ -0,0 +1,9 @@
+
+$('#toggle-menu').click('on',function() {
+  if ($('#menu-content2').css('display') == 'none'){
+    $('#menu-content2').css('display','block')
+  }
+  else{
+    $('#menu-content2').css('display','none')
+    }
+})
