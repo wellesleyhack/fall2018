b8b8da00a0c810f380697a103f13943c4891f944
wellesleyhack <hack@wellesley.edu>
2019-10-23 00:24:20 -0400

    change schedule button colors

--- static/js/index.js
+++ static/js/index.js
@@ -10,19 +10,19 @@ $(document).ready(function() {
 
   $("#saturday-toggle").click(function() {
     $('#saturday').css("display", "block");
-    $('#saturday-toggle').css("background-color", "#e77c91");
+    $('#saturday-toggle').css("background-color", "rgba(21,203,206,1)");
     $('#sunday').css("display", "none");
-    $('#sunday-toggle').css("background-color", "#F97352");
+    $('#sunday-toggle').css("background-color", "rgba(35,117,124,1)");
     $('#friday').css("display", "none");
     $('#friday-toggle').css("background-color", "#F97352");
   });
 
   $("#sunday-toggle").click(function() {
       $('#sunday').css("display", "block");
-      $('#sunday-toggle').css("background-color", "#e77c91");
+      $('#sunday-toggle').css("background-color", "rgba(21,203,206,1)");
       $('#friday').css("display", "none");
       $('#friday-toggle').css("background-color", "#F97352");
       $('#saturday').css("display", "none");
-      $('#saturday-toggle').css("background-color", "#F97352");
+      $('#saturday-toggle').css("background-color", "rgba(35,117,124,1)");
   });
 });
